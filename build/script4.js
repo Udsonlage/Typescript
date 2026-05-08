96f6eb436d8d8faa656e47e1221494b7b442a7f5
Udsonlage <udson.ramos@naimseguro.com.br>
2026-05-08 19:38:13 -0300

    estudo de tuplas

---
+++ build/script4.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let coisas = ["Udson", 25, true];
+coisas.push("Ramos", 26, true);
+coisas[2] = false;
+console.log(coisas);
+//# sourceMappingURL=script4.js.map
---
+++ src/script4.ts
@@ -0,0 +1,6 @@
+let coisas: [string,number,boolean] = ["Udson", 25, true]
+coisas.push("Ramos",26,true)
+
+coisas[2] = false
+
+console.log(coisas)
